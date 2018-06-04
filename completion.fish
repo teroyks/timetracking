40ac0daf4f6c25e44f5188a90449bf4766bf90c4
Tero Ykspetäjä <tero.ykspetaja@codeworks.fi>
2018-06-04 17:06:59 +0300

    Add autocomplete and man entry for the 'add' command

--- completion.fish
+++ completion.fish
@@ -1,6 +1,7 @@
 complete --command timetracking --exclusive --short-option p --long-option project --arguments "(timetracking list)" --description "select project"
 complete --command timetracking --no-files --short-option c --long-option continue --description "also continue tracking other projects"
 complete --command timetracking --no-files --arguments "start end list"
+complete --command timetracking --no-files --arguments add --description "add project to project list"
 complete --command timetracking --no-files --short-option a --long-option active --description "only active projects"
 
 complete --command timetracking --no-files --short-option h --long-option help --description "show help"
